4a1a7e16eb961c9de6966733bfbd471133f73282
MediSmoke420 <jonesgreg662@gmail.com>
2025-06-29 23:02:00 +0100

    products-1.js

--- scripts/products-1.js
+++ scripts/products-1.js
@@ -3,60 +3,30 @@ const products = [
     title: "Electric Herb Grinder",
     price: "£19.99",
     image: "https://m.media-amazon.com/images/I/61y5zOT6cNL._AC_SL1500_.jpg",
-    url: "https://www.amazon.co.uk/dp/B08S38J7V3?tag=amazon06014-21"
+    url: "https://www.amazon.co.uk/dp/B07X1XSWKT?tag=amazon06014-21"
   },
   {
     title: "Glass Bong with Ice Catcher",
     price: "£29.95",
-    image: "https://m.media-amazon.com/images/I/61dZAzKJXqL._AC_SL1500_.jpg",
-    url: "https://www.amazon.co.uk/dp/B0BJ5PL3FV?tag=amazon06014-21"
+    image: "https://m.media-amazon.com/images/I/61dZAkXJqXL._AC_SL1500_.jpg",
+    url: "https://www.amazon.co.uk/dp/B08CXTVMTH?tag=amazon06014-21"
   },
   {
     title: "Rolling Tray - Metal",
     price: "£7.49",
-    image: "https://m.media-amazon.com/images/I/61FUEJZW8zL._AC_SL1001_.jpg",
-    url: "https://www.amazon.co.uk/dp/B07VCWTKJS?tag=amazon06014-21"
+    image: "https://m.media-amazon.com/images/I/61FUEJZWBzL._AC_SL1001_.jpg",
+    url: "https://www.amazon.co.uk/dp/B07CWTK3ZS?tag=amazon06014-21"
   },
   {
     title: "Smell Proof Storage Bag",
     price: "£22.99",
-    image: "https://m.media-amazon.com/images/I/71sY97ibHqL._AC_SL1500_.jpg",
-    url: "https://www.amazon.co.uk/dp/B0C6LNVX1G?tag=amazon06014-21"
+    image: "https://m.media-amazon.com/images/I/71sV971bHqL._AC_SL1500_.jpg",
+    url: "https://www.amazon.co.uk/dp/B08CGJ2D7F?tag=amazon06014-21"
   },
   {
     title: "Portable Vaporizer - Dry Herb",
     price: "£59.99",
-    image: "https://m.media-amazon.com/images/I/71XjX2NHhYL._AC_SL1500_.jpg",
-    url: "https://www.amazon.co.uk/dp/B08ZDNBVBB?tag=amazon06014-21"
+    image: "https://m.media-amazon.com/images/I/51G7qGQyNwL._AC_SL1500_.jpg",
+    url: "https://www.amazon.co.uk/dp/B09XZ8CLZJ?tag=amazon06014-21"
   },
-  {
-    title: "Ceramic Rolling Tray",
-    price: "£14.99",
-    image: "https://m.media-amazon.com/images/I/81GkwnvQjoL._AC_SL1500_.jpg",
-    url: "https://www.amazon.co.uk/dp/B08J4BPR1C?tag=amazon06014-21"
-  },
-  {
-    title: "Silicone Bong with Glass Bowl",
-    price: "£24.99",
-    image: "https://m.media-amazon.com/images/I/61M7RzLa10L._AC_SL1001_.jpg",
-    url: "https://www.amazon.co.uk/dp/B07ZH5XYTY?tag=amazon06014-21"
-  },
-  {
-    title: "Mini Smell Proof Case",
-    price: "£12.99",
-    image: "https://m.media-amazon.com/images/I/71RfvwM3CjL._AC_SL1500_.jpg",
-    url: "https://www.amazon.co.uk/dp/B07VLW4X9T?tag=amazon06014-21"
-  },
-  {
-    title: "Digital Pocket Scale",
-    price: "£8.99",
-    image: "https://m.media-amazon.com/images/I/71Kn+p4rjQL._AC_SL1500_.jpg",
-    url: "https://www.amazon.co.uk/dp/B08LNZ6Q2K?tag=amazon06014-21"
-  },
-  {
-    title: "XL Storage Tin – Airtight",
-    price: "£17.95",
-    image: "https://m.media-amazon.com/images/I/81jcV2L+ihL._AC_SL1500_.jpg",
-    url: "https://www.amazon.co.uk/dp/B08QRZWLTV?tag=amazon06014-21"
-  }
 ];
